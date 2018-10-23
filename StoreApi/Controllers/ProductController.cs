6e24579eeca6cd532bd9bb05cdfb8d54e5dbbbd8
amitbmark <amitbmark@gmail.com>
2018-10-23 21:16:05 +0300

    Added http requetes

--- StoreApi/Controllers/ProductController.cs
+++ StoreApi/Controllers/ProductController.cs
@@ -3,25 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using StoreApi.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace StoreApi.Controllers
 {
+    [Route("api/[controller]")]
     public class ProductController : Controller
     {
         // GET: /<controller>/
-        [HttpGet]
+
         public IActionResult Get()
         {
-            Product product = MockData.GetData();
+            List<Product> products = MockData.GetData();
+            string json = JsonConvert.SerializeObject(products);
+
+            return Ok(json);
+        }
+
+
+       [HttpGet("(id)")]
+        public IActionResult Get(int id)
+        {
+            Product product = MockData.GetProductById(id);
+            string json = JsonConvert.SerializeObject(product);
+            return Ok(json);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Product product)
+        {
+            MockData.AddProduct(product);
             return Ok();
         }
 
-        public IActionResult Poop
+        [HttpDelete]
+        public IActionResult Delete([FromBody]int id)
         {
-
+            bool product = MockData.RemoveProduct(id);
+            return Ok();
         }
+
+
+       
     }
 }
--- StoreApi/Models/MockData.cs
+++ StoreApi/Models/MockData.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace StoreApi.Models
 {
     public static class MockData
     {
+        private static int Id { get; set; } = 0;
 
-        public static Product GetData(){
-            return new Product() { Id = 1, Name = "Amit", Description = "Amit is a smart guy", Price = 500 };
+        private static List<Product> products = new List<Product>();
+
+
+        public static void AddProduct(Product product)
+        {
+            Id++;
+            product.Id = Id;
+            products.Add(product);
         }
+
+        public static Product RemoveProduct(Product product)
+        {
+            products.Remove(product);
+            return product;
+        }
+
+        public static bool RemoveProduct(int id)
+        {
+        
+            products.Remove(GetProductById(id));
+            return true;
+        }
+
+
+        public static Product GetProductById(int id)
+        {
+            Product product = products.Where(producta => producta.Id == id).FirstOrDefault();
+            return product;
+        }
+
+
+        public static List<Product> GetData()
+        {
+            return products;
+        }
+
+
+
 
     }
 }
